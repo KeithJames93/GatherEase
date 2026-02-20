aa502413e2b812e0b4cb6c3ee8f6486a3525b50e
Keith Giordano <kjamesgiordano@gmail.com>
2026-02-20 22:11:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7686669059-4ee6b",
+  "appId": "1:622005558708:web:8129f9fa975a16628d66e1",
+  "apiKey": "AIzaSyDeGP5Df-unw09FWVEaOlRAwfRbRWIG-NI",
+  "authDomain": "studio-7686669059-4ee6b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "622005558708"
+};
